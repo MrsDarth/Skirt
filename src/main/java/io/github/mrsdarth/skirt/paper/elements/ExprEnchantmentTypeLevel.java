2d0039f4832cfe86184150ee7d67ee59320c443d
Mrs-Darth <7jgbm234@gmail.com>
2021-12-03 22:14:28 +0800

    Skirt 2

---
+++ src/main/java/io/github/mrsdarth/skirt/paper/elements/ExprEnchantmentTypeLevel.java
@@ -0,0 +1,28 @@
+package io.github.mrsdarth.skirt.paper.elements;
+
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.util.EnchantmentType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ExprEnchantmentTypeLevel extends SimplePropertyExpression<EnchantmentType, Long> {
+
+    static {
+        register(ExprEnchantmentTypeLevel.class, Long.class, "enchant[ment] level", "enchantmenttypes");
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "enchantment level";
+    }
+
+    @Override
+    public @Nullable Long convert(EnchantmentType enchantmentType) {
+        return (long) enchantmentType.getLevel();
+    }
+
+    @Override
+    public @NotNull Class<? extends Long> getReturnType() {
+        return Long.class;
+    }
+}
