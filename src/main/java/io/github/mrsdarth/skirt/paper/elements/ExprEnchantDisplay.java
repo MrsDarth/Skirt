e741ce3c03729d2671e388418ab10ac89e24d3c9
Mrs-Darth <7jgbm234@gmail.com>
2021-12-31 15:28:59 +0800

    fix translations

--- src/main/java/io/github/mrsdarth/skirt/paper/elements/ExprEnchantDisplay.java
+++ src/main/java/io/github/mrsdarth/skirt/paper/elements/ExprEnchantDisplay.java
@@ -5,7 +5,9 @@ import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.util.EnchantmentType;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import io.github.mrsdarth.skirt.Reflectness;
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.renderer.TranslatableComponentRenderer;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.translation.GlobalTranslator;
 import net.kyori.adventure.translation.TranslationRegistry;
@@ -17,8 +19,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.InputStreamReader;
 import java.text.MessageFormat;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 @Name("Enchant display")
 @Description("returns an enchantment type as a formatted string example sharpness 7 returns §7Sharpness VII")
@@ -28,20 +29,17 @@ import java.util.ResourceBundle;
 
 public class ExprEnchantDisplay extends SimplePropertyExpression<EnchantmentType, String> {
 
+    private static TranslatableComponentRenderer<Locale> RENDERER;
+
     static {
         try {
             Module module = Bukkit.class.getModule();
             TranslationRegistry registry = TranslationRegistry.create(Key.key("skirt", "mojang-translations"));
             registry.registerAll(Locale.US, ResourceBundle.getBundle("mojang-translations/en_US", module), false);
-            GlobalTranslator.get().addSource(registry);
-            GlobalTranslator.get().addSource(new Translator() {
+            RENDERER = TranslatableComponentRenderer.usingTranslationSource(new ChainingTranslator(registry) {
                 private final JsonObject json = JsonParser.parseReader(new InputStreamReader(module.getResourceAsStream("assets/minecraft/lang/en_us.json"))).getAsJsonObject();
                 @Override
-                public @NotNull Key name() {
-                    return Key.key("skirt", "minecraft-lang");
-                }
-                @Override
-                public @Nullable MessageFormat translate(@NotNull String key, @NotNull Locale locale) {
+                public @Nullable MessageFormat trans(@NotNull String key, @NotNull Locale locale) {
                     return json.has(key) ? new MessageFormat(json.get(key).getAsString(), locale) : null;
                 }
             });
@@ -60,11 +58,29 @@ public class ExprEnchantDisplay extends SimplePropertyExpression<EnchantmentType
     @Override
     public String convert(EnchantmentType enchantmentType) {
         Enchantment type = enchantmentType.getType();
-        return (type == null) ? null : LegacyComponentSerializer.legacySection().serialize(GlobalTranslator.render(type.displayName(enchantmentType.getLevel()), Locale.US));
+        return (type == null) ? null : LegacyComponentSerializer.legacySection().serialize(RENDERER.render(type.displayName(enchantmentType.getLevel()), Locale.US));
     }
 
     @Override
     public @NotNull Class<? extends String> getReturnType() {
         return String.class;
     }
+
+    private static abstract class ChainingTranslator implements Translator {
+        private final Translator chain;
+        public ChainingTranslator(Translator chain) {
+            this.chain = chain;
+        }
+        @Override
+        public @NotNull Key name() {
+            return Key.key("skirt");
+        }
+        @Override
+        public @Nullable MessageFormat translate(@NotNull String key, @NotNull Locale locale) {
+            MessageFormat format = trans(key, locale);
+            return format == null ? chain.translate(key, locale) : format;
+        }
+        protected abstract MessageFormat trans(@NotNull String key, @NotNull Locale locale);
+    }
+
 }
